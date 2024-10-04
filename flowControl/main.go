3cd407a21d16cc5afd66078fcf92d221e89b8b25
Никита Рыжакин <nikitaryzakin@MacBook-Pro-Nikita.local>
2024-10-04 20:04:42 +0300

    cycle if else/if else and initialization

--- flowControl/main.go
+++ flowControl/main.go
@@ -1,12 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	kayakPrice := 275.00
-	if kayakPrice > 500 {
-		fmt.Println("Price is greeater than 500")
-	} else if kayakPrice < 300 {
-		fmt.Println("Price is less than 300")
+	priceString := "275"
+	if kayakPrice, err := strconv.Atoi(priceString); err == nil {
+		fmt.Println("Price:", kayakPrice)
+	} else {
+		fmt.Println("Error:", err)
 	}
+
 }
