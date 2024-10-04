d3cb1ae2224be7dbea4610484e3a5482697957ad
Никита Рыжакин <nikitaryzakin@MacBook-Pro-Nikita.local>
2024-10-04 20:52:08 +0300

    label operator

--- flowControl/main.go
+++ flowControl/main.go
@@ -6,16 +6,11 @@ import (
 )
 
 func main() {
-	for counter := 0; counter < 10; counter++ {
-		switch {
-		case counter == 0:
-			fmt.Println("Zero value")
-		case counter < 3:
-			fmt.Println(counter, "is < 3")
-		case counter >= 3 && counter < 7:
-			fmt.Println(counter, "is more or equally 3 but less 7")
-		default:
-			fmt.Println(counter, "is more or equally 7")
-		}
+	count := 0
+target:
+	fmt.Println("Counter -", count)
+	count++
+	if count < 5 {
+		goto target
 	}
 }
