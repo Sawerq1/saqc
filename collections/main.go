05860138b4776374a869c20f07a679bae124d6d9
Никита Рыжакин <nikitaryzakin@MacBook-Pro-Nikita.local>
2024-10-05 20:39:02 +0300

    influence of capacity in slice

--- collections/main.go
+++ collections/main.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	names := [3]string{"Kayak", "Lifejacket", "Paddle"}
-	for _, name := range names {
-		fmt.Println("Value:", name)
-	}
-
+	names := make([]string, 3, 6)
+	names[0] = "Kayak"
+	names[1] = "Lifejacket"
+	names[2] = "Padding"
+	appendedNames := append(names, "Hat", "Gloves")
+	names[0] = "Canoe"
+	fmt.Println(names)
+	fmt.Println(appendedNames)
 }
