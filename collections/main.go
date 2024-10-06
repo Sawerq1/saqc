f57238ae94a028fb16044422c44c64089145fc31
Никита Рыжакин <nikitaryzakin@MacBook-Pro-Nikita.local>
2024-10-06 22:14:20 +0300

    slice to array

--- collections/main.go
+++ collections/main.go
@@ -1,15 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	//"reflect"
+)
 
 func main() {
-	products := [4]string{"Kayak", "Lifejacket", "Paddle", "Hat"}
-	someNames := products[1:3:3]
-	allNames := products[:]
-	someNames = append(someNames, "Gloves")
-	someNames = append(someNames, "Boots")
-	fmt.Println(someNames)
-	fmt.Println("Len of someNames-", len(someNames), "Cap of someNames-", cap(someNames))
-	fmt.Println(allNames)
-	fmt.Println("Len of allNames-", len(allNames), "Cap of allNames-", cap(allNames))
+	p1 := []string{"Kayak", "Lifejacket", "Paddle", "Hat"}
+	arrayPtr := (*[3]string)(p1)
+	array := *arrayPtr
+	fmt.Println(array)
 }
