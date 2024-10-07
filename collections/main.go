447dbba7f20c5b07f8cd6efc97756bbc51cfe8d7
Никита Рыжакин <nikitaryzakin@MacBook-Pro-Nikita.local>
2024-10-07 21:42:40 +0300

    Map and some operations with them

--- collections/main.go
+++ collections/main.go
@@ -3,11 +3,33 @@ package main
 import (
 	"fmt"
 	//"reflect"
+	"sort"
 )
 
 func main() {
-	p1 := []string{"Kayak", "Lifejacket", "Paddle", "Hat"}
-	arrayPtr := (*[3]string)(p1)
-	array := *arrayPtr
-	fmt.Println(array)
+	products := map[string]float64{
+		"Kayak":      279,
+		"Lifejacket": 48.95,
+		"Hat":        0,
+	}
+	fmt.Println("map size:", len(products))
+	//delete(products, "Hat")
+	if _, ok := products["Hat"]; ok {
+		fmt.Println("stored")
+	} else {
+		fmt.Println("not stored")
+	}
+	fmt.Printf("\n\n")
+	for key, value := range products {
+		fmt.Println("Key:", key, "Value:", value)
+	}
+	fmt.Printf("\n\n\nSorted map->\n")
+	keys := make([]string, 0, len(products))
+	for key := range products {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	for _, key := range keys {
+		fmt.Println("Key:", key, "Value:", products[key])
+	}
 }
